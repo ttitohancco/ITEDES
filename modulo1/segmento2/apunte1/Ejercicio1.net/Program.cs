b2b032098dd76b63d51df0fdb8d3aa001d1e0122
ttitohancco <answerttito@gmail.com>
2018-10-04 21:52:03 -0300

    clase html javascrip .net

---
+++ modulo1/segmento2/apunte1/Ejercicio1.net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio1.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("como te llamas: ");
+	    string name = Console.ReadLine();
+
+	    Console.WriteLine("hola " + name);
+        }
+    }
+}
